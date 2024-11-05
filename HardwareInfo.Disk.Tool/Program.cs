89f39af5cc15e8200a9355c12f27e11322d57550
usausa <machi.pon@gmail.com>
2024-11-05 16:43:47 +0900

    Update tool

--- HardwareInfo.Disk.Tool/Program.cs
+++ HardwareInfo.Disk.Tool/Program.cs
@@ -11,79 +11,96 @@ rootCommand.Handler = CommandHandler.Create((IConsole console) =>
 {
     foreach (var disk in DiskInfo.GetInformation())
     {
-        console.WriteLine($"Disk-{disk.Index}: {disk.Model}");
-        console.WriteLine($"  BusType: {disk.BusType}");
-        console.WriteLine($"  SmartType: {disk.SmartType}");
+        console.WriteLine($"Disk-{disk.Index} {disk.Model}");
+        console.WriteLine($"  BusType        : {disk.BusType}");
+        console.WriteLine($"  SmartType      : {disk.SmartType}");
 
-        console.WriteLine($"  Size: {disk.Size:#,0}");
-        console.WriteLine($"  BytesPerSector: {disk.BytesPerSector:#,0}");
+        console.WriteLine($"  Size           : {disk.Size:#,0}");
+        console.WriteLine($"  BytesPerSector : {disk.BytesPerSector:#,0}");
 
-        console.WriteLine("  Partition:");
+        console.WriteLine($"  Partitions     : {disk.Partitions}");
         foreach (var partition in disk.GetPartitions())
         {
-            console.WriteLine($"    Partition-{partition.Index}: {partition.Name}");
+            console.WriteLine($"    Partition-{partition.Index} : {partition.Name}");
             foreach (var drive in partition.Drives)
             {
                 var used = drive.Size - drive.FreeSpace;
-                console.WriteLine($"      Drive {drive.Name.TrimEnd(':')}: {used:#,0} / {drive.Size:#,0} ({(double)used * 100 / drive.Size:F2}%)");
+                console.WriteLine($"      Drive[{drive.Name}] : {used:#,0} / {drive.Size:#,0} ({(double)used * 100 / drive.Size:F2}%)");
             }
         }
 
         if (disk.SmartType == SmartType.Nvme)
         {
             var smart = (ISmartNvme)disk.Smart;
-            console.WriteLine("  SMART:");
-            console.WriteLine($"    CriticalWarning: 0x{smart.CriticalWarning:X2}");
-            console.WriteLine($"    Temperature: {smart.Temperature}");
-            console.WriteLine($"    AvailableSpare: {smart.AvailableSpare}");
-            console.WriteLine($"    AvailableSpareThreshold: {smart.AvailableSpare}");
-            console.WriteLine($"    PercentageUsed: {smart.PercentageUsed}");
-            console.WriteLine($"    DataRead: {smart.DataUnitRead * 512 * 1000 / 1024 / 1024 / 1024}");
-            console.WriteLine($"    DataWrite: {smart.DataUnitWrite * 512 * 1000 / 1024 / 1024 / 1024}");
-            console.WriteLine($"    HostReadCommand: {smart.HostReadCommand}");
-            console.WriteLine($"    HostWriteCommand: {smart.HostWriteCommand}");
-            console.WriteLine($"    ControllerBusyTime: {smart.ControllerBusyTime}");
-            console.WriteLine($"    PowerCycle: {smart.PowerCycle}");
-            console.WriteLine($"    PowerOnHour: {smart.PowerOnHour}");
-            console.WriteLine($"    UnsafeShutdown: {smart.UnsafeShutdown}");
-            console.WriteLine($"    MediaError: {smart.MediaError}");
-            console.WriteLine($"    ErrorInfoLogEntry: {smart.ErrorInfoLogEntry}");
-            console.WriteLine($"    WarningCompositeTemperatureTime: {smart.WarningCompositeTemperatureTime}");
-            console.WriteLine($"    CriticalCompositeTemperatureTime: {smart.CriticalCompositeTemperatureTime}");
+
+            var rows = new List<KeyValuePair<string, string>>
+            {
+                new("CriticalWarning", $"{smart.CriticalWarning:X2}"),
+                new("Temperature", $"{smart.Temperature}"),
+                new("AvailableSpare", $"{smart.AvailableSpare}"),
+                new("AvailableSpareThreshold", $"{smart.AvailableSpare}"),
+                new("PercentageUsed", $"{smart.PercentageUsed}"),
+                new("DataRead(GB)", $"{smart.DataUnitRead * 512 * 1000 / 1024 / 1024 / 1024}"),
+                new("DataWrite(GB)", $"{smart.DataUnitWrite * 512 * 1000 / 1024 / 1024 / 1024}"),
+                new("HostReadCommand", $"{smart.HostReadCommand}"),
+                new("HostWriteCommand", $"{smart.HostWriteCommand}"),
+                new("ControllerBusyTime", $"{smart.ControllerBusyTime}"),
+                new("PowerCycle", $"{smart.PowerCycle}"),
+                new("PowerOnHour", $"{smart.PowerOnHour}"),
+                new("UnsafeShutdown", $"{smart.UnsafeShutdown}"),
+                new("MediaError", $"{smart.MediaError}"),
+                new("ErrorInfoLogEntry", $"{smart.ErrorInfoLogEntry}"),
+                new("WarningCompositeTemperatureTime", $"{smart.WarningCompositeTemperatureTime}"),
+                new("CriticalCompositeTemperatureTime", $"{smart.CriticalCompositeTemperatureTime}")
+            };
             for (var i = 0; i < smart.TemperatureSensors.Length; i++)
             {
                 var value = smart.TemperatureSensors[i];
                 if (value > 0)
                 {
-                    console.WriteLine($"    TemperatureSensors-{i}: {value}");
+                    rows.Add(new($"TemperatureSensors-{i}", $"{value}"));
                 }
             }
+
+            console.WriteLine("  SMART:");
+            ShowSmartTable(rows);
         }
         else if (disk.SmartType == SmartType.Generic)
         {
             var smart = (ISmartGeneric)disk.Smart;
-            console.WriteLine("  SMART:");
+
+            var rows = new List<KeyValuePair<string, string>>();
             foreach (var id in smart.GetSupportedIds())
             {
-                switch (id)
+                var attr = smart.GetAttribute(id);
+                if (attr.HasValue)
                 {
-                    case SmartId.Temperature:
-                        ShowValue(smart, id, static x => x & 0xFF);
-                        break;
-                    default:
-                        ShowValue(smart, id, static x => x);
-                        break;
+                    switch (id)
+                    {
+                        case SmartId.Temperature:
+                            rows.Add(new(ToKey(id), ToValue(attr.Value, static x => x & 0xFF)));
+                            break;
+                        default:
+                            rows.Add(new(ToKey(id), ToValue(attr.Value, static x => x)));
+                            break;
+                    }
                 }
             }
+
+            console.WriteLine("  SMART:");
+            ShowSmartTable(rows);
+
+            string ToKey(SmartId id) => Enum.IsDefined(id) ? $"{(byte)id:X2} {id}" : $"{(byte)id:X2} Undefined";
+
+            string ToValue<T>(SmartAttribute attr, Func<ulong, T> converter) => $"{attr.RawValue:X12} {converter(attr.RawValue)}";
         }
 
-        void ShowValue<TResult>(ISmartGeneric smart, SmartId id, Func<ulong, TResult> converter)
+        void ShowSmartTable(List<KeyValuePair<string, string>> rows)
         {
-            var attr = smart.GetAttribute(id);
-            if (attr.HasValue)
+            var maxKey = rows.Max(static x => x.Key.Length);
+            foreach (var row in rows)
             {
-                var name = Enum.IsDefined(id) ? $"{(byte)id:X2} {id}" : $"{(byte)id:X2} Undefined";
-                console.WriteLine($"    {name}: {attr.Value.RawValue:X12} {converter(attr.Value.RawValue)}");
+                console.WriteLine($"    {row.Key.PadRight(maxKey)} : {row.Value}");
             }
         }
     }
